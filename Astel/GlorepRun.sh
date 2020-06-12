8b05187b929e6ec038f904c45cba72097cabe38f
DeashenReddy <DeashenReddy@github.com>
2020-06-12 15:56:04 +0200

    Partial Commit with new Approach

--- Astel/GlorepRun.sh
+++ Astel/GlorepRun.sh
@@ -2,4 +2,5 @@
 
 
 # GloRep Test Cases
-robot -o Login_Out.xml -d Results/Glorep/LoginDMSRun --variable SITENAME:GloRep --variable BROWSER:chrome ./TestSuites/Login.robot
+# robot -o Login_Out.xml -d Results/Glorep/LoginGlorepRun --variable SITENAME:GloRep --variable BROWSER:chrome ./TestSuites/Login.robot
+robot -o Reseller_Out.xml -d Results/Glorep/ResellerGlorepRun --variable SITENAME:GloRep --variable BROWSER:chrome ./TestSuites/Reseller.robot
