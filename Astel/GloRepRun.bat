a82907168b1a25089c3e3c619b37be259aaf1273
Leona <leonanaidoo15@gmail.com>
2020-06-25 10:53:13 +0200

    Final Quote Changes

--- Astel/GloRepRun.bat
+++ Astel/GloRepRun.bat
@@ -3,4 +3,5 @@ python --version
 
 REM REM GloRep Test Cases
 REM robot -o Login_Out.xml -d Results/Glorep/Login --variable SITENAME:GloRep --variable BROWSER:chrome ./TestSuites/Login.robot
-robot -o Customer_Out.xml -d Results/Glorep/Customer --variable SITENAME:GloRep --variable BROWSER:chrome ./TestSuites/Customer.robot
+REM robot -o Customer_Out.xml -d Results/Glorep/Customer --variable SITENAME:GloRep --variable BROWSER:chrome ./TestSuites/Customer.robot
+robot -o Customer_Out.xml -d Results/Glorep/Quotes --variable SITENAME:GloRep --variable BROWSER:chrome ./TestSuites/Quotes.robot
